ad2d8e88deb0144b0fdf7584e193e901a747791a
Evgeniy <60233967+mifeex@users.noreply.github.com>
2020-02-20 13:58:19 +0400

    Now bot can check the last questions an locked it

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let scrape = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://professional-secure.justanswer.com/');
+    // await page.setRequestInterception(true);
     // Код для скрапинг
 
     const result = await page.evaluate(() => {
@@ -20,30 +21,42 @@ let scrape = async () => {
         }
     });
 
-    await page.click('#remember');
+    async function puppeteerMutationListener(addedText, index) {
+        console.log(`Added text: ${addedText}`);
+
+        const link = await page.$(`body > div.question-list > div.bodyContainer > div.list-body-panel > div.questionlist > ul > div.scroll-questions-container > li:nth-child(${index + 2}) > div.question.open > div.question__info-and-hide > div.question__info-container > div > div.question__reply.chatbutton`);  
+        const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page()))); 
+
+        await link.click();
+        const newPage = await newPagePromise;
+
+        await newPage.bringToFront();
+
+        setTimeout(() => {
+            newPage.click('body > div.chat-container > div.main > div > div.expert-reply-region > div > div.pro-response > div > div > div.post-status > div.question-actions.js-question-lock-region > div > p.relock-question.js-lock');
+        }, 15000)     
+    }
+
     await page.click('#modal-login > div.form > form > div.submit > button');    
 
-    // const openFirstQuestion = () => {
+    await page.waitForSelector('body > div.question-list > div.bodyContainer > div.list-body-panel > div.questionlist > ul > div.scroll-questions-container');
+    // await page.click('#openQuestionsButton');
 
+    page.exposeFunction('puppeteerMutationListener', puppeteerMutationListener);
 
-    //     const questionCount = page.evaluate(() => {
-    //         let questions = document.querySelectorAll('')
-    //         link.click(questions[0]);
-    //     });
-    // }
-    await page.waitForSelector('body > div.question-list > div.bodyContainer > div.list-body-panel > div.questionlist > ul > div.scroll-questions-container > li:nth-child(2) > div.question.open > div.question__info-and-hide > div.question__info-container > div > div.question__reply.chatbutton');
-    const link = await page.$('body > div.question-list > div.bodyContainer > div.list-body-panel > div.questionlist > ul > div.scroll-questions-container > li:nth-child(2) > div.question.open > div.question__info-and-hide > div.question__info-container > div > div.question__reply.chatbutton');  
-    const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page()))); 
+    await page.evaluate(() => {
+        const mutationObserver = new MutationObserver((mutationsList) => {
+            for (const mutation of mutationsList) {
+                // if (!mutationsList.contains('')) {}
+                window.puppeteerMutationListener(mutation.addedNodes[0].textContent, document.querySelector('#priorityCountMark').innerText);
+            }
+          });
 
-    await link.click();
-    const newPage = await newPagePromise;
-
-    await newPage.waitForSelector('body > div.chat-container > div.main > div > div.expert-reply-region > div > div.pro-response > div > div > div.post-status > div.question-actions.js-question-lock-region > div > p.relock-question.js-lock');
-    await newPage.bringToFront();     
-    await newPage.click('body > div.chat-container > div.main > div > div.expert-reply-region > div > div.pro-response > div > div > div.post-status > div.question-actions.js-question-lock-region > div > p.relock-question.js-lock');
+        mutationObserver.observe(document.querySelector("body > div.question-list > div.bodyContainer > div.list-body-panel > div.questionlist > ul > div.scroll-questions-container"), {childList: true});
+    });
 
     // // Возврат значения
-    return {result};
+    return {result, puppeteerMutationListener};
     browser.close();
 
 };
@@ -51,3 +64,4 @@ let scrape = async () => {
 scrape().then((value) => {
     console.log(value); // Получилось!
 });
+
